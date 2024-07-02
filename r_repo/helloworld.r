0eccb9568245331f9de6e6d359350fe373bfd952
Ken Wang <ykw@pitt.edu>
2024-07-02 21:26:21 +0800

    Updated R script

---
+++ r_repo/helloworld.r
@@ -0,0 +1,12 @@
+# This is a demo file for R script
+
+# Installing a new package
+# install.packages("dplyr")
+
+# Import the new package to R
+library(dplyr)
+
+# Setting up the work directory
+setwd("/Users/kwang/GitHub/summer2024/r_repo")
+
+print("hello world")
